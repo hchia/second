6da58871e42147a2391fd8d71be1d26099217ac7
hchia <hchia@comp.nus.edu.sg>
2015-01-27 17:00:11 +0800

    init commit

---
+++ sieve.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define MAX 100
+
+void print(int notprimes[])
+{
+   int i;
+
+   for (i = 2; i < MAX; i++)
+   {
+      if (!notprimes[i])
+         printf("%2d ", i);
+   }
+
+   printf("\n");
+   getchar();
+   return;
+}
+
+int main(void)
+{  
+   int notprimes[MAX+1] = {0};
+   int i, j;
+
+   for (i = 2; i < MAX; i++)
+   {
+      for (j = 2; j <= i; j++)
+      {
+         if (!notprimes[j])
+         {
+            if (i*j >= MAX)
+               break;
+
+            printf("i=%d,j=%d\n", i, j);
+            notprimes[j*i] = 1;
+            print(notprimes);
+         }
+
+         if (i % j == 0)
+            break;
+      }
+   }
+
+   return 0;
+}
